dee83c7b4a4dbcd5614799724e49fc5cbf5757ce
singhxayush <fanduayush2000@gmail.com>
2024-09-17 09:06:51 +0530

    idk

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,12 @@ process.on('warning', (warning) => {
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const articleRoutes = require('./articleRoutes');
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
